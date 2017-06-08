b64106f26e3b733067485be407af355234c149e4
Gihong Kim <gyong1211@gmail.com>
2017-06-08 21:10:13 +0900

    edit post's models - delete CommentInfo, edit Comment(change to MTO model)

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class User(models.model):
+class User(models.Model):
     name = models.CharField(max_length=40)
     nickname = models.CharField(max_length=40)
+
+    def __str__(self):
+        return '{}({})'.format(self.nickname,self.name)
--- django_app/post/models.py
+++ django_app/post/models.py
@@ -11,31 +11,46 @@ member application 생성
 # author, user 필드는 제외하고 생성
 
 class Post(models.Model):
+    author = models.ForeignKey(
+        'member.User',
+        on_delete=models.CASCADE
+    )
     image = models.ImageField()
     content = models.TextField()
-    comment = models.ManyToManyField(
-        'Comment',
-        through='CommentInfo'
+    tag = models.ManyToManyField(
+        'Tag',
     )
+
+    def __str__(self):
+        return '{}의 포스트 : {}'.format(self.author.nickname, self.content)
 
 
 class Comment(models.Model):
-    content = models.TextField()
+    author = models.ForeignKey(
+        'member.User',
+        on_delete=models.CASCADE,
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+    )
+    content = models.CharField(max_length=120)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}의 포스트 {}에 대한 {}의 댓글 : {}'.format(self.post.author.nickname, self.post.content, self.author.nickname, self.content)
 
 
 class PostLike(models.Model):
+    user = models.ForeignKey(
+        'member.User',
+    )
     like = models.PositiveIntegerField(default=0)
 
 
 class Tag(models.Model):
-    post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE
-    )
+    content=models.CharField(max_length=20)
 
-
-class CommentInfo(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
-    created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.content
