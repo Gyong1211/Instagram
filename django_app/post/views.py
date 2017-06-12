18a1c43ae7ece81d10b08e507acea1ba33e3a695
Gihong Kim <gyong1211@gmail.com>
2017-06-12 23:59:33 +0900

    add post_delete(views, urls, templates)

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -82,3 +82,13 @@ def post_modify(request, post_pk):
             return redirect('post_detail', post_pk=post.id)
 
 
+def post_delete(request, post_pk):
+    # post_pk에 해당하는 Post에 대한 delete요청만을 받음
+    # 처리완료후에는 post_list페이지로 redirect
+    post = Post.objects.get(id=post_pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+
+    elif request.method == 'GET':
+        return render(request, 'post/post_delete.html')
