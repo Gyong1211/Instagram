691de400643eeb4ed17581a269ce864f2d1b9f6b
Gihong Kim <gyong1211@gmail.com>
2017-06-13 12:19:15 +0900

    set STATIC_DIR, STATICFILES_DIRS

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -41,6 +41,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS =[
+    STATIC_DIR,
+]
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -149,7 +160,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
