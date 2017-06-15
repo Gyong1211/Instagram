6134f3e7d15b4cf55c23b284e730ab3c892b5ef8
Gihong Kim <gyong1211@gmail.com>
2017-06-15 12:25:42 +0900

    add member login (url, view, template)

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login as django_login
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username'],
+        password = request.POST['password'],
+        user = authenticate(
+            username=username,
+            password=password
+        )
+        if user is not None:
+            django_login(request, user)
+            return redirect('post:post_list')
+        else:
+            return HttpResponse('Login invalid!')
+
+
+    else:
+        if request.user.is_autenticated:
+            return redirect('post:post_list')
+        return render(request, 'member/login.html')
