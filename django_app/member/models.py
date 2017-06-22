413bc571acf79632031a4b8f810a1409aea26ba0
Gihong Kim <gyong1211@gmail.com>
2017-06-22 14:55:52 +0900

    User 모델 follow, follower를 만들기 위해 중간자모델 Relation 생성 및 적용

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,6 +1,27 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+"""
+    동작
+        follow : 내가 다른사람을 follow함
+        unfollow : 내가 다른사람에게 한 follow를 취소함
+    속성
+        followers : 나를 follow하고 있는 사람들
+        follower : 나를 follow하고 있는 사람
+        following : 내가 follow하고 있는 사람들
+        friend : 나와 서로 follow하고 있는 관계
+        friends : 나와 서로 follow하고 있는 모든 관계
+        없음 : 내가 follow하고 있는 사람 1명
+            (나는 저 사람의 follower이다 또는 나는 저 사람을 follow하고 있다 라고 표현)
+    ex) 내가 박보영, 최유정, 고성현을 follow하고 고성현과 김수정은 나를 follow한다
+        나의 followers는 고성현, 김수정
+        나의 following은 박보영, 최유정
+        김수정은 나의 follower이다
+        나는 박보영의 follower다
+        나와 고성현은 friend관계이다
+        나의 friends는 고성현 1명이다
+"""
+
 
 class User(AbstractUser):
     nickname = models.CharField(
@@ -8,6 +29,43 @@ class User(AbstractUser):
         null=True,
         unique=True,
     )
+    relations = models.ManyToManyField(
+        'self',
+        through='Relation',
+        symmetrical=False,
+    )
 
     # def __str__(self):
     #     return self.nickname
+
+    def follow_toggle(self, user):
+        if not isinstance(user, User):
+            raise ValueError
+
+        relation, relation_created = self.follow_relations.get_or_create(to_user=user)
+
+        if not relation_created:
+            relation.delete()
+
+
+class Relation(models.Model):
+    from_user = models.ForeignKey(
+        User,
+        related_name='follow_relations',
+    )
+    to_user = models.ForeignKey(
+        User,
+        related_name='follower_relations',
+    )
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'Relation from {} to {}'.format(
+            self.from_user,
+            self.to_user,
+        )
+
+    class Meta:
+        unique_together = (
+            ('from_user', 'to_user')
+        )
