d5c775efb977c53397f40926e41ab3631028345d
Gihong Kim <gyong1211@gmail.com>
2017-06-27 10:41:32 +0900

    Youtube API를 이용해 search 결과를 youtube_search template에 출력

--- django_app/post/views/youtube.py
+++ django_app/post/views/youtube.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 
 __all__ = (
@@ -6,4 +7,22 @@ __all__ = (
 
 
 def youtube_search(request):
+    url_youtube_search = 'https://www.googleapis.com/youtube/v3/search'
+    YOUTUBE_API_ACCESS_TOKEN = 'AIzaSyAUvTn6g5D6DmX6dHk4_LfNY3rzH1wqRxA'
+    print(request.GET)
+    q = request.GET.get('q')
+    if q:
+        url_youtube_search_params = {
+            'part': 'snippet',
+            'key': YOUTUBE_API_ACCESS_TOKEN,
+            'q': q,
+            'maxResults': 5,
+            'type': 'video'
+        }
+        response = requests.get(url_youtube_search, params=url_youtube_search_params)
+        context = {
+            'response': response.json(),
+        }
+        return render(request, 'post/youtube_search.html', context=context)
+
     return render(request, 'post/youtube_search.html')
