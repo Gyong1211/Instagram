65afbaecbf957786a67d0105b10512e034b91511
Gihong Kim <gyong1211@gmail.com>
2017-07-11 14:22:16 +0900

    UserDetailView 추가

--- django_app/config/urls/urls_views.py
+++ django_app/config/urls/urls_views.py
@@ -24,7 +24,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^post/', include('post.urls.urls_views')),
-    url(r'^member/', include('member.urls')),
+    url(r'^member/', include('member.urls.urls_views')),
     url(r'^$', views.index, name='index')
     # url(r'^$', RedirectView.as_view(pattern_name='post:post_list')),
 ]
